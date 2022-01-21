b53a65f8a5120e1fbb3fed0c12cde664ecef1250
MattEnns <matthew.enns@dcmail.ca>
2022-01-21 17:39:28 -0500

    updated ICE 2

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,6 +9,35 @@
         {
             location.href = "about.html";
         });
+
+        // Step 1 - get a reference to an entry point(s) (insertion / deletion)
+        let MainContent = document.getElementsByTagName("main")[0];
+        let DocumentBody = document.body;
+
+        // Step 2 = Create a HTML Element in memory
+        let MainParagraph = document.createElement("p");
+        let Article = document.createElement("article");
+        let ArticleParagraph = `<p id="ArticleParagraph" class "mt-3"> This is the article paragraph</p>`;
+
+        // Step 3 - Configure new Element
+        MainParagraph.setAttribute("id", "MainParagraph");
+        MainParagraph.setAttribute("class", "mt-3");
+        let FirstString = "This is";
+        let SecondString = `${FirstString} the Main Paragraph`;
+        MainParagraph.textContent = SecondString;
+
+        // Step 4 - perform insertion / deletion
+
+        // example of insert after (append)
+        MainContent.appendChild(MainParagraph);
+        Article.innerHTML = ArticleParagraph;
+        DocumentBody.appendChild(Article);
+
+        // example of insert before
+        //MainContent.before(MainParagraph);
+
+        // example of deletion
+        AboutUsButton.remove();
     }
 
     // named function
