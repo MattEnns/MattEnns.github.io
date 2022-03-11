c4ccdcfdf086ece02118378e3a0085f9e057658e
MattEnns <matthew.enns@dcmail.ca>
2022-03-11 17:27:37 -0500

    ICE 8

--- Scripts/authguard.js
+++ Scripts/authguard.js
@@ -1,8 +1,12 @@
 "use strict";
-(function()
-{
-    if(!sessionStorage.getItem("user"))
-    {
-        location.href = "login.html";
+(function () {
+    let protected_routes = [
+        "contact-list"
+    ];
+    if (protected_routes.indexOf(router.ActiveLink) > -1) {
+        if (!sessionStorage.getItem("user")) {
+            location.href = "/login";
+        }
     }
 })();
+//# sourceMappingURL=authguard.js.map
--- Scripts/namespace.js
+++ Scripts/namespace.js
@@ -1,2 +1,3 @@
+
 "use strict";
-let core;
+//# sourceMappingURL=namespace.js.map
